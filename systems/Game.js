4175f268a5b2db3b997297f09515ffe0c7ff973e
jenrose24 <rosienifer@icloud.com>
2018-07-17 22:44:42 +0000

    automatically backed up by learn

--- systems/Game.js
+++ systems/Game.js
@@ -68,6 +68,7 @@ AFRAME.registerSystem('game', {
     this.player.el.components.sound.playSound()
     this.removeAllBalls()
     // TODO: spawn a new ball!
+    this GAME.spawnball()
   },
 
   newBallShouldSpawn: function() {
